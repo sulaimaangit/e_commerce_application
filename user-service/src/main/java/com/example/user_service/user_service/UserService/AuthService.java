36d67c6a0cae8f446258333e9a9e5175dd4131d6
sulaimaangit <sulaimaan.works@gmail.com>
2026-09-24 20:23:19 +0530

    Add endpoint to list users

--- user-service/src/main/java/com/example/user_service/user_service/UserService/AuthService.java
+++ user-service/src/main/java/com/example/user_service/user_service/UserService/AuthService.java
@@ -3,6 +3,8 @@ package com.example.user_service.user_service.UserService;
 import com.example.user_service.user_service.dto.AuthResponse;
 import com.example.user_service.user_service.dto.LoginRequest;
 import com.example.user_service.user_service.dto.RegisterRequest;
+import com.example.user_service.user_service.dto.UserResponse;
+import java.util.List;
 
 public interface AuthService {
 
@@ -11,4 +13,6 @@ public interface AuthService {
     AuthResponse login(LoginRequest request);
 
     AuthResponse currentUser(String email);
+
+    List<UserResponse> allUsers();
 }
